bc42b3533a4c5d8316edeb8fe3120219412d787c
n3xus_ <whatnexus@vk.com>
2025-02-24 00:26:19 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from 'next'
-import { Montserrat } from 'next/font/google'
+import { Montserrat, Montserrat_Alternates } from 'next/font/google'
 import { ReactNode } from 'react'
 
 import '@/app/assets/app.css'
 
 const montserrat = Montserrat({
 	variable: '--font-montserrat',
+	weight: ['400', '500', '600', '700', '800'],
+	subsets: ['latin', 'cyrillic'],
+})
+
+const montserratAlternates = Montserrat_Alternates({
+	variable: '--font-montserrat-alternates',
+	weight: ['400', '500', '600', '700', '800'],
 	subsets: ['latin', 'cyrillic'],
 })
 
@@ -24,7 +31,7 @@ export default function RootLayout (
 	return (
 		<html className="dark" lang="ru">
 		<body
-			className={`${montserrat.variable} antialiased`}
+			className={`${montserrat.variable} ${montserratAlternates.variable} antialiased`}
 		>
 		{children}
 		</body>
